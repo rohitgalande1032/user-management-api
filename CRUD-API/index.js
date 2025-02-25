c5b4c31fe89f5b40702b5cac553bd656118b6c17
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-25 11:19:11 +0530

    update user, delete user

--- CRUD-API/index.js
+++ CRUD-API/index.js
@@ -33,10 +33,68 @@ app.post("/api/users", async (req, res) => {
 })
 
 //get users route
-app.get("/api/users", (req, res) => {
-    res.send("This is the get users route")
+app.get("/api/users", async (req, res) => {
+    try{
+        const users = await User.find()
+        res.status(200).json(users)
+    }catch(err) {
+        res.status(500).json({message: err.message})
+    }
 })
 
+//dynamic routing
+//querying
+//query parameter are used to filter the data
+
+app.get("/api/users/:id", async (req ,res) => {
+    try {
+        const {id} = req.params
+        const user = await User.findById(id)
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Update the user
+app.put("/api/users/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        //serve first find the user based on the id
+        //update necessary information after extracting it from body
+        const user = await User.findByIdAndUpdate(id, req.body)
+
+        //if user not found handle this error
+        if(!user) {
+            return res.status(404).json({message: "User not found"})
+        }
+
+        //this is to confirm if the changes have been implemented or not
+        const updatedUser = await User.findById(req.params.id);
+        res.status(200).json(updatedUser)
+
+    } catch (error) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+
+//delete the user
+app.delete("/api/users/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        //delete the user based on the id
+        const user = await User.findByIdAndDelete(id)
+        
+        if(!user) {
+            return res.status(404).json({message: "User not found"})
+        }
+        res.status(200).json(user)
+        res.status(200).json({"message" : "User deleted successfully!!"})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
